ef361d5eb60c7f47e432ec5a14e08999a33d108a
Kanak Biscuitwala <kanak.b@hotmail.com>
2013-04-08 17:10:17 -0700

    Initial commit

---
+++ src/mobisocial/socialkitdemo/DemoActivity.java
@@ -0,0 +1,202 @@
+package mobisocial.socialkitdemo;
+
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import mobisocial.socialkit.Obj;
+import mobisocial.socialkit.musubi.DbFeed;
+import mobisocial.socialkit.musubi.DbIdentity;
+import mobisocial.socialkit.musubi.Musubi;
+import mobisocial.socialkit.obj.MemObj;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.NavUtils;
+import android.support.v4.view.ViewPager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class DemoActivity extends FragmentActivity {
+    
+    private static final String TAG = "DemoActivity";
+    
+    private static final String ACTION_CREATE_FEED = "musubi.intent.action.CREATE_FEED";
+    private static final int REQUEST_CREATE_FEED = 1;
+
+    /**
+     * The {@link android.support.v4.view.PagerAdapter} that will provide
+     * fragments for each of the sections. We use a
+     * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
+     * will keep every loaded fragment in memory. If this becomes too memory
+     * intensive, it may be best to switch to a
+     * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+     */
+    SectionsPagerAdapter mSectionsPagerAdapter;
+
+    /**
+     * The {@link ViewPager} that will host the section contents.
+     */
+    ViewPager mViewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo);
+
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the app.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(
+                getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.pager);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.demo, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+        case R.id.create_feed:
+            if (!Musubi.isMusubiInstalled(this)) {
+                Log.d(TAG, "Musubi not installed!");
+                return super.onOptionsItemSelected(item);
+            }
+            String action = ACTION_CREATE_FEED;
+            int request = REQUEST_CREATE_FEED;
+            Intent intent = new Intent(action);
+            startActivityForResult(intent, request);
+            return true;
+        default:
+            return super.onOptionsItemSelected(item);
+        }
+    }
+    
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_CREATE_FEED && resultCode == RESULT_OK) {
+            if (data == null || data.getData() == null) {
+                return;
+            }
+            
+            Uri feedUri = data.getData();
+            Log.d(TAG, "feedUri: " + feedUri.toString());
+            
+            Musubi musubi = Musubi.getInstance(this);
+            
+            DbFeed feed = musubi.getFeed(feedUri);
+            if (feed == null) {
+                Log.d(TAG, "feed is null!?!?");
+                return;
+            }
+            
+            List<DbIdentity> members = feed.getMembers();
+            for (DbIdentity member : members) {
+                Log.d(TAG, "member: " + member.getName() + ", " + member.getId());
+            }
+
+            JSONObject json = new JSONObject();
+            try {
+                json.put("can't see this", "invisible");
+                json.put(Obj.FIELD_HTML, "hi");
+                
+            } catch (JSONException e) {
+                Log.e(TAG, "json error", e);
+                return;
+            }
+            feed.postObj(new MemObj("socialkitdemo", json));
+        }
+    }
+
+    /**
+     * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
+     * one of the sections/tabs/pages.
+     */
+    public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+        public SectionsPagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            // getItem is called to instantiate the fragment for the given page.
+            // Return a DummySectionFragment (defined as a static inner class
+            // below) with the page number as its lone argument.
+            Fragment fragment = new DummySectionFragment();
+            Bundle args = new Bundle();
+            args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            // Show 3 total pages.
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            Locale l = Locale.getDefault();
+            switch (position) {
+            case 0:
+                return getString(R.string.title_section1).toUpperCase(l);
+            case 1:
+                return getString(R.string.title_section2).toUpperCase(l);
+            case 2:
+                return getString(R.string.title_section3).toUpperCase(l);
+            }
+            return null;
+        }
+    }
+
+    /**
+     * A dummy fragment representing a section of the app, but that simply
+     * displays dummy text.
+     */
+    public static class DummySectionFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        public static final String ARG_SECTION_NUMBER = "section_number";
+
+        public DummySectionFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_demo_dummy,
+                    container, false);
+            TextView dummyTextView = (TextView) rootView
+                    .findViewById(R.id.section_label);
+            dummyTextView.setText(Integer.toString(getArguments().getInt(
+                    ARG_SECTION_NUMBER)));
+            return rootView;
+        }
+    }
+
+}
